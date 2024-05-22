a16b692a200410908489ba699131f2e1432b60d2
vipin_760 <vipinm500@gmail.com>
2024-05-22 19:01:42 +0530

    create page setup

---
+++ frontend/src/components/Partials/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <div className='main bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 w-full p-2 text-center hover:bg-indigo-900'>
+     <h2 className='font-body text-white shadow-lg'>all copyrights reserved @ </h2>
+      </div>
+    </>
+  )
+}
+
+export default Footer
