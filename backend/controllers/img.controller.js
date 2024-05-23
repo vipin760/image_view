47c03b287366075d88ad231a5319474e9ff51d9c
sreeshma <sreeshma.t.2019@gmail.com>
2024-05-23 15:35:12 +0530

    not needed

--- backend/controllers/img.controller.js
+++ backend/controllers/img.controller.js
@@ -6,9 +6,11 @@ exports.createImg = async (req, res) => {
         const newImage = await Image.create(req.body);
         return res
             .status(200)
-            .send({ status:true,message: "Successfully created the image" });
+            .send({ status: true, message: "Successfully created the image" });
     } catch (error) {
-        return res.status(500).send({status:false, message: `${error.message}` });
+        return res
+            .status(500)
+            .send({ status: false, message: `${error.message}` });
     }
 };
 
@@ -20,10 +22,14 @@ exports.getAllImage = async (req, res) => {
                 .status(200)
                 .send({ message: "All images found", data: images });
         } else {
-            return res.status(400).send({status:true, message: "No images found" });
+            return res
+                .status(400)
+                .send({ status: true, message: "No images found" });
         }
     } catch (error) {
-        return res.status(500).send({status:false, message: `${error.message}` });
+        return res
+            .status(500)
+            .send({ status: false, message: `${error.message}` });
     }
 };
 exports.updateImage = async (req, res) => {
@@ -35,13 +41,21 @@ exports.updateImage = async (req, res) => {
         });
         console.log(req.params, "ID");
         if (!img) {
-            return res.status(400).send({status:true, message: "There is no images" });
+            return res
+                .status(400)
+                .send({ status: true, message: "There is no images" });
         }
         return res
             .status(200)
-            .send({status:true, message: "successfully updated images", data: img });
+            .send({
+                status: true,
+                message: "successfully updated images",
+                data: img,
+            });
     } catch (error) {
-        return res.status(500).send({status:false, message: `${error.message}` });
+        return res
+            .status(500)
+            .send({ status: false, message: `${error.message}` });
     }
 };
 exports.deleteImage = async (req, res) => {
@@ -50,8 +64,10 @@ exports.deleteImage = async (req, res) => {
         await Image.findByIdAndDelete(req.params.tha);
         return res
             .status(200)
-            .send({status:true, message: "The image deleted successfully" });
+            .send({ status: true, message: "The image deleted successfully" });
     } catch (error) {
-        return res.status(500).send({status:false message: `${error.message}` });
+        return res
+            .status(500)
+            .send({ status: false, message: `${error.message}` });
     }
 };
--- frontend/src/components/Pages/Home/Home.jsx
+++ frontend/src/components/Pages/Home/Home.jsx
@@ -1,33 +1,68 @@
-import './Home.css'
-import React from 'react'
+import { useEffect, useState } from "react";
+import "./Home.css";
+import axios from "axios";
+import { toast } from "react-toastify";
+const [image, setImage] = useState();
+const handleDelete = (id) => {
+    axios
+        .delete(`http://localhost:3000/api/v2/image/${id}`)
+        .then((res) => {
+            toast.success(res.data.message);
+        })
+        .catch((error) => {
+            toast.eroor(error.response.data.message);
+        });
+};
 
 const Home = () => {
-  return (
-    <section className='main_card'>
-    <div className="py-2">
-        <div className="mx-auto max-w-6xl text-gray-500">
-            <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-3">
-                
-                <div className="relative group object-contain p-1 rounded-2xl bg-white border border-gray-200 dark:border-gray-800 dark:bg-gray-900">
-                    <img className='rounded-2xl' src="https://plus.unsplash.com/premium_photo-1669562729042-b0a3d23afcf1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGZvcmVzdHxlbnwwfHwwfHx8MA%3D%3D" alt="" />
-                    <div className="relative">
-                        <div className="mt-1 pb-1 rounded-b-[--card-border-radius]">
-                          <h3 className='wallpaper_title text-2xl'>wall papper</h3>
-                            <p className="text-gray-700 dark:text-gray-300">Amet praesentium deserunt ex commodi tempore fuga voluptatem. Sit, sapiente.</p>
-                            <div className='flex gap-2'>
-                              <a href="/edit/1542515151" className='bg-indigo-500 hover:bg-indigo-600 text-white rounded pl-2 pr-2'><button>Edit</button></a>
-                              <a href="" className='bg-pink-500 hover:bg-pink-600 text-white rounded pl-2 pr-2'><button>Delete</button></a>
+    return (
+        <section className="main_card">
+            <div className="py-2">
+                <div className="mx-auto max-w-6xl text-gray-500">
+                    <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-3">
+                        <div className="relative group object-contain p-1 rounded-2xl bg-white border border-gray-200 dark:border-gray-800 dark:bg-gray-900">
+                            <img
+                                className="rounded-2xl"
+                                src="https://plus.unsplash.com/premium_photo-1669562729042-b0a3d23afcf1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGZvcmVzdHxlbnwwfHwwfHx8MA%3D%3D"
+                                alt=""
+                            />
+                            <div className="relative">
+                                <div className="mt-1 pb-1 rounded-b-[--card-border-radius]">
+                                    <h3 className="wallpaper_title text-2xl">
+                                        wall papper
+                                    </h3>
+                                    <p className="text-gray-700 dark:text-gray-300">
+                                        Amet praesentium deserunt ex commodi
+                                        tempore fuga voluptatem. Sit, sapiente.
+                                    </p>
+                                    <div className="flex gap-2">
+                                        <a
+                                            href="/edit/1542515151"
+                                            className="bg-indigo-500 hover:bg-indigo-600 text-white rounded pl-2 pr-2"
+                                        >
+                                            <button>Edit</button>
+                                        </a>
+                                        <a
+                                            href=""
+                                            className="bg-pink-500 hover:bg-pink-600 text-white rounded pl-2 pr-2"
+                                        >
+                                            <button
+                                                onClick={() =>
+                                                    handleDelete(data._id)
+                                                }
+                                            >
+                                                Delete
+                                            </button>
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
-            
             </div>
-        </div>
-    </div>
-</section>
-  )
-}
+        </section>
+    );
+};
 
-export default Home
+export default Home;
